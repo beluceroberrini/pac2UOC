638c9324cb8fd66865193f6be01eac9212c98215
beluceroberrini <99675685+beluceroberrini@users.noreply.github.com>
2023-01-19 15:53:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const button = document.querySelector('#button');
+
 let miPrompt1;
 let miPrompt2;
 
@@ -17,6 +19,7 @@ console.log(n1)
 let n2 = parseInt(miPrompt2, 10);
 console.log(n2)
 
+
 //SUMA
 
 function sum(n1, n2) {
@@ -24,94 +27,109 @@ function sum(n1, n2) {
 }
 
 let result = sum(n1, n2);
-console.log(sum(n1, n2));
 
 //PRIMOS
-// function isPrime(number) {
-//     for (let factor = 2; factor < number; factor++) {
-//         if (number % factor === 0)
-//             return false
-//     }
-//     return true
-// }
 
-// console.log(isPrime(n1), isPrime(n2))
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++) {
+        if (number % factor === 0)
+            return false
+    }
+    return true
+}
 
-// function printPrimos(n1, n2){
-    
-//         let resultPrimos = '';
-//         let primo1, primo2;
+console.log(isPrime(n1), isPrime(n2))
 
-//     if(!isPrime(n1) && !isPrime(n2)){
-//         resultPrimos += 'Ninguno es primo'
-//     }else if(isPrime(n1) && isPrime(n2)){
-//         resultPrimos += 'Ambos son primos'
-//     }
-    
-//     primo1 = isPrime(n1) ? `${n1} es primo` : `${n1} no es primo`; 
-//     primo2 = isPrime(n2) ? `${n2} es primo` : `${n2} no es primo`;
+function printPrimos(n1, n2) {
 
-//     if (resultPrimos === ''){
-//         resultPrimos += `${primo1} y ${primo2}`;
-    
-//     }
-//     return resultPrimos;
-// }
+    let resultPrimos = '';
+    let primo1, primo2;
 
-// console.log(printPrimos(n1, n2))
-// let primos = printPrimos(n1,n2);
+    if (!isPrime(n1) && !isPrime(n2)) {
+        resultPrimos += 'Ninguno es primo'
+    } else if (isPrime(n1) && isPrime(n2)) {
+        resultPrimos += 'Ambos son primos'
+    } else if (isPrime(n1) === isPrime(n2)) {
+        resultPrimos += `${primo1}`
+    }
+
+    primo1 = isPrime(n1) ? `${n1} es primo` : `${n1} no es primo`;
+    primo2 = isPrime(n2) ? `${n2} es primo` : `${n2} no es primo`;
+
+    if (resultPrimos === '') {
+        resultPrimos += `${primo1} y ${primo2}`;
+
+    }
+    return resultPrimos;
+}
+
+console.log(printPrimos(n1, n2))
+let primos = printPrimos(n1, n2);
 
 
 //PARES
 
-function showPares(){
-    
+function parImpar(number) {
+    if (number % 2 === 0) {
+        return `${number} es par`
+    }
+    return `${number} es impar`
+}
+
+function showPares(n1, n2){
+ 
     let resultPares = '';
 
-    function parImpar(number) {
-        if (number % 2 === 0) {
-            return `${number} es par`
-        }
-        return `${number} es impar`
-    }
-
-    function printImpares(limit){
-        for (let i = `${n2}`; i >= limit; i--){
-            if (!(i % 2 === 0))
-            console.log(i)
-            return i
-        }   
-    }
-
-    function printPares(limit){
-        for (let i = `${n1}`; i <= limit; i++){
-            if (i % 2 === 0)
-            console.log(i)
-            return i
-        }   
-    }
-
-
-    if ((n1) < (n2)){
-        resultPares += printPares(n2)
-    }else if ((n1) > (n2)){
-        resultPares += printImpares(n1)
-    }else if((n1) === (n2)){
+    if ((n1) === (n2)){
         resultPares += parImpar(n1)
     }else{
         resultPares += `${parImpar(n1)} y ${parImpar(n2)}`
     }
 
-    console.log(resultPares)
-    return resultPares;
+    return resultPares
 }
 
-console.log(showPares(n1, n2));
-let pares = showPares(n1,n2);
+let pares = showPares(n1, n2);
 
 
+function showArray() {
 
-alert(`La suma de ambos números es ${result} \n.\n${pares}`);
+    let arrayPares = [];
+
+    function printImpares(limit) {
+        for (let i = `${n1}`; i >= limit; i--) {
+            if (i % 2 === 1) {
+                console.log(i)
+                arrayPares.push(' ' + i)
+            }
+        }
+    }
+
+    function printPares(limit) {
+        for (let i = `${n1}`; i <= limit; i++) {
+            if (i % 2 === 0) {
+                console.log(i)
+                arrayPares.push(' ' + i)
+            }
+        }
+    }
+
+    if ((n1) < (n2)) {
+        arrayPares += printPares(n2)
+    } else if ((n1) > (n2)) {
+        arrayPares += printImpares(n2)
+    } else if ((n1) === (n2)) {
+        arrayPares += parImpar(n1)
+    }
+
+    return arrayPares
+
+}
+
+// console.log(showArray(n1, n2));
+let array = showArray(n1, n2);
+
+alert(`La suma de ambos números es ${result} \n${primos}.\n${pares}\n${array}`);
 
 // la suma es:
 // el n1 es primo y el n2 no es primo
